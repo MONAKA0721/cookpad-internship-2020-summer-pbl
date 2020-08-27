1d8365b8eb74ade31e042f0bf7315f970edf345a
Yuya Sumie <monamonacan0926@gmail.com>
2020-08-27 13:45:02 +0900

    recipes にダミーデータ投入

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
     has_many :steps, dependent: :destroy
+    has_many :ingredients, dependent: :destroy
+
+    accepts_nested_attributes_for :steps
+    accepts_nested_attributes_for :ingredients
+    validates :title, presence: true, length: { maximum: 255 }
 end
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Recipe.destroy_all
+
+recipes = []
+%w[
+  絶対失敗しないマカロン
+  簡単に作れる肉じゃが
+  美味しいアイスポックスクッキー
+  混ぜるだけ！ベイクドチーズケーキ
+  真っ白レアチーズケーキ
+  しっとりダックワーズ
+  基本のプレーンマフィン
+  豆腐ドーナツ
+  いつもの唐揚げ
+  時間外フィナンシェ
+].each do |recipe_title|
+  recipes << Recipe.create!(
+    title: recipe_title,
+    steps_attributes: rand(1..4).times.map do |i|
+      { description: "手順 #{i + 1}", position: i }
+    end,
+    ingredients_attributes: rand(1..4).times.map do |i|
+      { name: "材料 #{i + 1}", quantity: "#{i * 10}g" }
+    end
+  )
+end
