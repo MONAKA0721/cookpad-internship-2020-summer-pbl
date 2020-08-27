bd58395e5b72a346d461f371b0b683d609aef2fb
Yuya Sumie <monamonacan0926@gmail.com>
2020-08-27 20:39:45 +0900

    gon 導入と Chart.js 導入 栄養素グラフ表示

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,11 +6,23 @@ class RecipesController < ActionController::Base
     def show
         @recipe = Recipe.find(params[:id])
         @recommended_recipes = recommend_recipes_for(@recipe)
+        gon.nutritions = 
+            @recommended_recipes.map do |recipe|
+                types_to_values(@recipe.nutrition.to_set + recipe.nutrition.to_set)
+            end
     end
 
     private
         def recommend_recipes_for(recipe)        
             lack_nutrition_types = [0, 1, 2, 3, 4, 5] - recipe.nutrition
-            Recipe.find_by_lack_nutrition(lack_nutrition_types)
+            recipes = Recipe.find_by_lack_nutrition(lack_nutrition_types)
+        end
+
+        def types_to_values(types)
+            values = [0, 0, 0, 0, 0, 0]
+            types.to_a.each do |type|
+                values[type] = 5
+            end
+            values
         end
 end
